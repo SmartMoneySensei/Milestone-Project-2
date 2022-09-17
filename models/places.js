78d22203244363d3e185522ac9c27dcfbf1c8ae1
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-16 19:31:13 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,15 @@
+module.exports = [{
+    name:'Lousiana Fried Chicken & Mac Ribz',
+    city: 'Las Vegas',
+    state:'NV',
+    cuisines:'Chicken, Homestyle BBQ Ribs',
+    pic:'/images/bbq.jpeg'
+},
+{
+    name:'Mr. Fries Man',
+    city: 'Las Vegas',
+    state:'NV',
+    cuisines:'Honey Shrimp Fries, Steak or Chicken',
+    pic:'/images/MrFries.jpeg'
+}
+]
