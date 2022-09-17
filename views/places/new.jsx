9cef88d3789ef5a514f78b9639463377434369eb
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-16 19:08:28 -0700

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form () {
         <Def>
           <main>
             <h1>Add a New Place</h1>
+            <form method="POST" action="/places">
+              <div className="form-group">
+                <label htmlFor="name"><span class="required-field">*</span>Place Name</label>
+                <input className="form-control" id="name" name="name" required/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="pic">Place Picture</label>
+                <input className="form-control" id="pic" name="pic" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="city">City</label>
+                <input className="form-control" id="city" name="city" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="state">State</label>
+                <input className="form-control" id="state" name="state" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cuisines"><span class="required-field">*</span>Cuisines</label>
+                <input className="form-control" id="cuisines" name="cuisines" required/>
+              </div>
+              <input className="btn btn-success" type="submit" value="Add Restaurant"/>
+            </form>
           </main>
         </Def>
     )
