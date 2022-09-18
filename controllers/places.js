a9466b4298b017236e8e958b8afe1db53d90448f
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-18 10:26:47 -0700

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req,res) => {
-    let places = [{
-        name:'Lousiana Fried Chicken & Mac Ribz',
-        city: 'Las Vegas',
-        state:'NV',
-        cuisines:'Chicken, Homestyle BBQ Ribs',
-        pic:'/images/bbq.jpeg'
-    },
-    {
-        name:'Mr. Fries Man',
-        city: 'Las Vegas',
-        state:'NV',
-        cuisines:'Honey Shrimp Fries, Steak or Chicken',
-        pic:'/images/MrFries.jpeg'
-    }
-    ]
     res.render('places/index', {places})
 })
 
@@ -33,9 +19,20 @@ router.get('/show', (req,res) => {
 
 // Post Routes
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
   
 
 module.exports = router
