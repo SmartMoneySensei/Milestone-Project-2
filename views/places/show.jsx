aebab7e2f8c54a0359c37f8029a62d50fcdad69a
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-18 13:48:47 -0700

    Show route connected

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,47 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated!
+        </h3>
+    )
+
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
+            <div className="row">
+                <div className="col-sm-6">
+                    <img src={data.place.pic} alt={data.place.name} width="90%"/>
+                </div>
+                <div className="col-sm-6">
+                    <h1>{data.place.name}</h1>
+                    <h2> Rating </h2>
+                    {rating}
+                    <br/>
+                    <h2> Description</h2>
+                    <h3> Located in {data.place.city}, {data.place.state}</h3>
+                    <h4>Serving {data.place.cuisines}</h4>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <h2> Comments</h2>
+                {comments}
+            </div>
+            <a href="" className="btn btn-warning"> Edit</a>  
+            <form method="POST" action=""> 
+            <button type="submit" className="btn btn-danger">
+                Delete
+            </button>
+            </form>     
+
+
         </Def>
     )
 }
