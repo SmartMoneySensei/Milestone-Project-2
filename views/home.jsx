845ae380b72221080650f467186790a229c331da
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-25 13:24:19 -0700

    missing pics

--- views/home.jsx
+++ views/home.jsx
@@ -6,7 +6,7 @@ function home (){
         <Def>
             <main>
                 <h1>REST-Rant</h1>
-                <div >
+                <div>
                     <img src="/images/Free.jpg" alt="man surrounded by water" width="50%"/>
                     <div>
                     Photo by <a href="https://unsplash.com/@coopery?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mohamed Nohassi</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
