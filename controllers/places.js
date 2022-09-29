24192fe40953460e982470d218f3501250ad7fd0
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-28 20:57:58 -0700

    comment section

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,21 @@
 const router = require('express').Router()
 const db = require('../models')
 
-// Index Route
 router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', {places})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
-// Post Route
 router.post('/', (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
+  .then(() =>{
+    res.redirect('/places')
   })
   .catch(err => {
     if (err && err.name == 'ValidationError') {
@@ -26,7 +24,8 @@ router.post('/', (req, res) => {
           message += `${field} was ${err.errors[field].value}. `
           message += `${err.errors[field].message}`
       }
-      res.render('places/new', { message }, req.body.object)
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
   }
   else {
       res.render('error404')
@@ -34,17 +33,15 @@ router.post('/', (req, res) => {
   })
 })
 
-
-
-// New Route
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-// Show Route
 router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
+  db.Place.findOne({ _id: req.params.id})
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -53,24 +50,45 @@ router.get('/:id', (req, res) => {
   })
 })
 
-
-
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
 })
 
-// Delete Route
+router.get('/:id/edit', (req, res) => {
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+    console.log(place.comments)
+    res.render('places/edit', {place})
+  })
+  .catch (err => {
+    res.render('error404')
+  })
+})
+
 router.delete('/:id', (req, res) => {
   res.send('DELETE /places/:id stub')
 })
 
-// Edit Route
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
